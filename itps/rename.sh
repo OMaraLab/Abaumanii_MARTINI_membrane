a9527b186e72e45af51c60efbb872c2c703ee24a
hugo <hugomacdermott@gmail.com>
2020-11-23 16:07:18 +1100

    use actual MARTINI names

---
+++ itps/rename.sh
@@ -0,0 +1,18 @@
+find * -type f | xargs sed -i  's/D2PG/OIPG/g'
+find * -type f | xargs sed -i  's/D3PG/OAPG/g'
+find * -type f | xargs sed -i  's/D4PG/OUPG/g'
+find * -type f | xargs sed -i  's/D5PG/PUPG/g'
+find * -type f | xargs sed -i  's/D2PE/PUPE/g'
+find * -type f | xargs sed -i  's/D3PE/OUPE/g'
+# lowercase
+find * -type f | xargs sed -i  's/d2pg/OIPG/g'
+find * -type f | xargs sed -i  's/d3pg/OAPG/g'
+find * -type f | xargs sed -i  's/d4pg/OUPG/g'
+find * -type f | xargs sed -i  's/d5pg/PUPG/g'
+find * -type f | xargs sed -i  's/d2pe/PUPE/g'
+find * -type f | xargs sed -i  's/d3pe/OUPE/g'
+
+
+
+
+
